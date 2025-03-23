b21c3d5b24bbde5a5f69fa70e4b1b78377f21afd
s31426 <s31426@pjwstk.edu.pl>
2025-03-23 14:22:42 +0100

    com2

--- cw3abpd/cw3abpd/kontenerChlodniczy.cs
+++ cw3abpd/cw3abpd/kontenerChlodniczy.cs
@@ -1,6 +1,30 @@
 ﻿namespace cw3abpd;
 
-public class kontenerChlodniczy
+public class kontenerChlodniczy:kontener
 {
-    
+    public string typProduktu;
+    public double temp;
+
+    public kontenerChlodniczy(string typProduktu, double temp, double height, double containerWeight, double depth,
+        double maxCapacity)
+        : base(height, containerWeight, depth, maxCapacity, 'C')
+    {
+        this.typProduktu = typProduktu;
+        this.temp = temp;
+    }
+
+    public void setTemp(double temperature)
+    {
+        if (temperature < temp)
+        {
+            throw new Exception($"za niska temperatu {temperature}.");
+        }
+        this.temp = temperature;
+    }
+
+    public override void load(double mass)
+    {
+        base.load(mass);
+    }
+        
 }
---
+++ cw3abpd/cw3abpd/kontenerowiec.cs
@@ -0,0 +1,38 @@
+﻿namespace cw3abpd;
+
+public class kontenerowiec
+{
+    public string name;
+    public double maxSpeed;
+    public int maxCountConteiners;
+    public double maxWeight;
+    public List<kontener> kontenery = new List<kontener>();
+
+    public kontenerowiec(string name, double maxSpeed, int maxCountConteiners, double maxWeight)
+    {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.maxCountConteiners = maxCountConteiners;
+        this.maxWeight = maxWeight;
+    }
+
+    public void loadConteiners(kontener kontener)
+    {
+        if (kontenery.Count >= maxCountConteiners)
+        {
+            throw new Exception($"Too much conteiners for {name}");
+        }
+
+        double totalWeight = 0;
+        for (int i = 0; i < kontenery.Count; i++)
+        {
+            totalWeight += kontenery[i].containerWeight + kontenery[i].paylodeWeight;
+        }
+
+        if (totalWeight > maxWeight)
+        {
+            throw new Exception($"Too much weight for {name}");
+        }
+        kontenery.Add(kontener);
+    }
+}
